07283c4f930d880180e314865c39a6707b2deb47
Olawale Ibrahim <ibrahim.olawale@earthanalytics.no>
2022-12-06 09:50:37 +0100

    dynamic robot components added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Hello from './Hello';
+import CardList from './CardList';
 import 'tachyons';
+import { robots } from './robots';
 // import registerServiceWorker from './registerServiceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hello greeting={'Hello' + ' React Ninja'}/>
+    <CardList robots={robots}/>
   </React.StrictMode>
 );
 
