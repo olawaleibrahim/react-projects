8e0086375c5b7b1866c0648abe3f6c751d42f80b
Olawale Ibrahim <ibrahim.olawale@earthanalytics.no>
2022-12-06 15:18:32 +0100

    input, searchbox -> App components

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import { robots } from "./robots";
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ""
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots} />
+            </div>
+        );
+    }
+    
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import 'tachyons';
-import { robots } from './robots';
 // import registerServiceWorker from './registerServiceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CardList robots={robots}/>
+    <App />
   </React.StrictMode>
 );
 
