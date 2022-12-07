920f9a069594d9a08b7c5a14e700dcaea0ebc24c
Olawale Ibrahim <ibrahim.olawale@earthanalytics.no>
2022-12-07 09:38:33 +0100

    add componentDidMount method

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
 import "./App.css";
 
 class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ""
         }
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users").then(
+                response=> response.json()).then(
+                users => this.setState({ robots: users }))
     }
 
     onSearchChange = (event) => {
@@ -21,13 +26,17 @@ class App extends React.Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
-        return (
-            <div className="tc">
-                <h1 className="f2">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f2">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots} />
+                </div>
+            );
+        }
     }
     
 }
